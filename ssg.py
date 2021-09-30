788196c443eb20762b8aaf40254e4dbc56fc0664
Alpinism75 <56472609+Alpinism75@users.noreply.github.com>
2021-09-30 21:56:09 +0100

    following corrections

--- ssg.py
+++ ssg.py
@@ -4,8 +4,7 @@ from ssg.site import Site
 
 
 def main(source="content", dest="dist"):
-    config={source:"source", dest:"dest"}
-    site = Site(**config)
+    config={"source": source, "dest": dest}
+    Site(**config).build()
 
-if __name__ == "__main__":
-    typer.run(main())
+typer.run(main)
