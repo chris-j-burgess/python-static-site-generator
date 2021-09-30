416f581091088b6d4fe2262ede93feca615a590e
Alpinism75 <56472609+Alpinism75@users.noreply.github.com>
2021-09-30 21:42:12 +0100

    creating site class

---
+++ ssg/site.py
@@ -0,0 +1,20 @@
+
+
+from os import mkdir
+from pathlib import Path
+
+class Site():
+    
+    def __init__(self, source, dest)
+        self.source = Path(source)
+        self.dest = Path(dest)
+
+    def create_dir(self):
+        directory = self.dest"/"relative_to(self.source)
+        mkdir(directory, parents=True, exist_ok=True)
+
+    def build(self):
+        mkdir(self.dest, parents=True, exist_ok=True)
+        for path in self.source.rglob("*")
+            if isdir(path):
+                create_dir(path)
